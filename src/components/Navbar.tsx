52bf819f5a4c2fd281175e28349ef11d48837d32
ajeebtech <jatintheboy@gmail.com>
2025-12-08 15:34:49 +0530

    iii

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,10 @@ import styles from '../app/page.module.css';
 export default function Navbar() {
     return (
         <nav className={styles.navbar}>
+            <div className={styles.navLogo}>
+                <span>PVP</span>
+                <span>VALORANT</span>
+            </div>
             <div className={styles.navLinks}>
                 <Link href="/" className={styles.navLink}>Home</Link>
                 <Link href="/data" className={styles.navLink}>Data</Link>
